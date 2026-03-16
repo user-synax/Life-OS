7d9597cfc75205fbdd00549523e1c25f426fa216
User Synax <usersynax@gmail.com>
2026-03-17 00:55:08 +0530

    Fix missing cn import in TopNav

--- src/components/layout/TopNav.jsx
+++ src/components/layout/TopNav.jsx
@@ -16,6 +16,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import useAuthStore from '@/store/useAuthStore';
 import useNotificationStore from '@/store/useNotificationStore';
 import NotificationPanel from '@/components/notifications/NotificationPanel';
+import { cn } from '@/lib/utils';
 
 export default function TopNav() {
   const [search, setSearch] = useState('');
