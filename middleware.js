e50acbaa508ba437bcfa194221025ef7acc66cdf
user-synax <usersynax@gmail.com>
2026-04-29 17:57:47 +0530

    Fix 3 307 error on production

--- middleware.js
+++ middleware.js
@@ -54,5 +54,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|_rsc).*)'],
 };
